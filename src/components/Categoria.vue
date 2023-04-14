900f8058e1ee0f16ed01d880c8723c73230ea9ce
Pedro-UP <pedrocazorla03@gmail.com>
2023-04-14 11:50:32 -0600

    Disenno y funciones de vue

---
+++ src/components/Categoria.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- //* Usaremos los siguientes parametros para mostrar la api
+   * idCategory, strCategory, strCategoryDescription, strCategoryThumb -->
+  <div class="category_container">
+       <!-- //* Para mostrar la imagen. -->
+    <img v-bind:src="category.strCategoryThumb" alt="" />
+       <!-- //* Para mostrar el nombre de la categoria -->
+    <h6>{{ category.strCategory }}</h6>
+        <!-- //* Para mostrar la descripcion -->
+    <p>{{ category.strCategoryDescription }}</p>
+  </div>
+</template>
+
+<script>
+export default{
+    props:["category"]
+}
+</script>
---
+++ src/components/Meal.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="category_container">
+       <!-- //* Para mostrar la imagen. -->
+    <img v-bind:src="meal.strMealThumb" alt="" />
+       <!-- //* Para mostrar el nombre del platillo -->
+    <p><b>{{ meal.strMeal }}</b></p>
+        <!-- //* Para mostrar la categoria y el origen -->
+    <h5>Categoria: {{ meal.strCategory }} | Origen: {{ meal.strArea}}</h5>
+  </div>
+</template>
+
+<script>
+export default{
+    props:["meal"]
+}
+</script>
